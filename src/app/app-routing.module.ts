163d446e2389641ceb56a88da436ca25b00d1252
killian-sweeney <killianmsweeney@gmail.com>
2023-09-09 19:13:45 -0400

    Angular Playground Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './component/home/home.component';
+import { ListComponent } from './component/list/list.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },  
+  { path: 'list', component: ListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
